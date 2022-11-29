7a942d112727d54c71b600089818356a5907aa34
ayoub2096 <ayoub.benlamin.7984@stu.he-ferrer.eu>
2022-11-29 12:22:54 +0100

    scrolltrigger -> toggleclass

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
-function init(){
+/* function init(){
     
 }
 
 window.addEventListener('load', function(){
     init();
+});
+ */
+gsap.registerPlugin(ScrollTrigger);
+$( document ).ready(function() {
+    //console.log( "ready!" );
+   
+/* gsap.to('#intro img', {
+    x:-900,
+    scrollTrigger: {
+        trigger: '#intro',
+        start: 'top top',
+        end:'bottom center',
+        scrub:true,
+        markers:true,
+    }
+    }
+) */
+
+gsap.set('#project02', {
+    scrollTrigger:{
+        trigger: '#project02 img',
+        start: 'top bottom-=40%',
+        end:'bottom center-=10%',
+        scrub:true,
+        toggleClass:{targets: "#project02", className: "active"},
+        markers:true,
+    }
+})
+
+
+
 });
